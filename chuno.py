243b5d0c9d20da4a461f0b9c341ab67180554b5c
edwinachan <edwina.chan@bbc.co.uk>
2019-06-18 08:14:34 +0100

    Update chuno to take user input
    - The game takes user input instead of playing through automatically
    - Accounts for user input errors and guides the user to play the game correctly

--- chuno.py
+++ chuno.py
@@ -48,12 +48,16 @@ class Deck:
     def pop(self):
         #Check if empty. If empty, last card in discard pile remains, but we take rest of pile, shuffle, and that now becomes new mydeck
         return self.cards.pop()
-
-
-    def dealCards(self):
+    
+    def dealCards(self, numOfPlayers):
         for i in range(initalHandSize):
-            for j in range(len(players)):
+            for j in range(numOfPlayers):
                 players[j].addCardToHand(self.cards.pop())
+    
+    def createPlayers(self, numOfPlayers):
+        for i in range(1, numOfPlayers + 1):
+            players.append(Player(input('Name player ' + str(i) + ': ')))
+        print('\n')
         
             
 class Player:
@@ -68,23 +72,30 @@ class Player:
         if len(self.hand) == 0:
             return True
         else:
-            print("Top of discard pile: " + str(discardPile[-1]))
+            print("Top of discard pile: " + str(discardPile[-1])+ '\n')
 
-        for card in self.hand:
+        playOrNot = input('Do you wish to play (type: play) or pick up (type: pick) a card? ')
+
+        if playOrNot == 'pick':
+            self.addCardToHand(myDeck.pop())
+            print("No matches made for " + self.name)
+            print(str(self.hand[-1]) + " from the deck has been added to the hand\n")
+            return False
+        elif playOrNot == 'play':
+            indexOfCard = int(input('What\'s the position of the card you want to play? ')) - 1
+            card = self.hand[indexOfCard]
             if card.colour == discardPile[-1].colour or card.number == discardPile[-1].number:
+                print(self.name + ' has matched with ' + str(card))
                 self.hand.remove(card)
                 discardPile.append(card)
-                print(self.name + " has matched " + str(card))
-                print("Remaining cards: " + str([str(self.hand[i]) for i in range(len(self.hand))]) + "\n")
+                print('Remaining cards: '+ str([str(self.hand[i]) for i in range(len(self.hand))]) + "\n")
                 if len(self.hand) == 0:
                     return True
                 else:
                     return False
-            
-        self.addCardToHand(myDeck.pop())
-        print("No matches made for " + self.name)
-        print(str(self.hand[-1]) + " from the deck has been added to the hand\n")
-        return False
+            else:
+                print('No match was made, next player will go.\n')
+                return False
 
     def __str__(self):
         s = ""
@@ -101,18 +112,25 @@ class Game:
         while won == False:
             playerGoing = (playerGoing + 1) % len(players)
             print(str(players[playerGoing].name) + " is going")
-            print(str(players[playerGoing].name) + " has:\n" + str(players[playerGoing]))
+            #print(str(players[playerGoing].name) + " has:\n" + str(players[playerGoing]))
+            print(str(players[playerGoing].name) + " has:\n")
+            playersHand = players[playerGoing].hand
+            listOfCards = [str(i+1) + ': ' + str(playersHand[i]) + '\n' for i in range(len(playersHand))]
+            print(''.join(listOfCards))
             won = players[playerGoing].haveGo(myDeck)
             #Needs to return True in order to break out of while loop
             if won == True:
                 print(str(players[playerGoing].name) + " has won")
             
-   
+
 #Play the game
-players = [Player('Ed'),Player('Eddie'),Player('Chan')]                
+print('Welcome to Chuno! The rules of the game are simple, and pretty much identical to Uno.\n 1) The aim of the game is to get rid of all your cards!\n 2) Each player starts with two cards in their hand. \n 3) You can play if a card in your hand matches the a) colour or b) number of the top card on the discard pile. Doing so removes the card from your hand. \n 4) If you can\'t go, you must pick up a card from the deck. \n 5) Repeat until someone\'s hand is empty. \nHave fun!\n') 
+numPlayers = int(input('How many players are there? '))
+players = []      
 myDeck = Deck()
 myDeck.shuffle()
-myDeck.dealCards()
+myDeck.createPlayers(numPlayers)
+myDeck.dealCards(numPlayers)
 discardPile = []
 discardPile.append(myDeck.pop()) #Putting this right after we've dealt
 #the cards as we only want to pop from myDeck at the start of the game
